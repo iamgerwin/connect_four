926a7e21d392036f19044a0d17bc4c30a0e896a6
John Gerwin De las Alas <iamgerwin@aim.com>
2016-05-01 00:22:31 +0800

    Complete necessary codes

--- app/assets/javascripts/c4.js
+++ app/assets/javascripts/c4.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
   function getSelectedButton() {
+    var names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh'];
     for (var i=1;i<8;i++) {
-      if ($('#column'+i+'_row1').hasClass("ui white circular label") ||
-            $('#column'+i+'_row1').hasClass("ui white circular label")) {
-              $('#column'+i+'_button').addClass('disabled');
+      if ($('#column'+i+'_row1').hasClass("ui white circular label") || $('#column'+i+'_row1').hasClass("ui black circular label")) {
+              $('#'+names[i-1]+'_column_button').addClass('disabled');
       }
     }
   }
@@ -17,28 +17,30 @@ $(document).ready(function(){
   }
 
   function checkHorizontalConnect() {
-    for(var i=6;i>3;x--) {
-      for(var j=1;y<3;j++) {
-        if ($('#column'+j+'_row'+x).hasClass("ui white circular label") &&
-            $('#column'+Number(j+1)+'_row'+x).hasClass("ui white circular label") &&
-            $('#column'+Number(j+2)+'_row'+x).hasClass("ui white circular label") &&
-            $('#column'+Number(j+3)+'_row'+x).hasClass("ui white circular label") &&
+    for(var i=6;i>3;i--) {
+      for(var j=1;j<3;j++) {
+        if ($('#column'+j+'_row'+i).hasClass("ui white circular label") &&
+            $('#column'+Number(j+1)+'_row'+i).hasClass("ui white circular label") &&
+            $('#column'+Number(j+2)+'_row'+i).hasClass("ui white circular label") &&
+            $('#column'+Number(j+3)+'_row'+i).hasClass("ui white circular label")
           ) {
               $('.ui.teal.compact.message').hide();
               $('.ui.center.aligned.icon.header').hide();
               disableButton();
               $('#winner').replaceWith("<h1><strong>White Player Wins!</strong></h1>");
+              $('#status').replaceWith('White Wins!');
           }
 
-        if ($('#column'+j+'_row'+x).hasClass("ui black circular label") &&
-            $('#column'+Number(j+1)+'_row'+x).hasClass("ui black circular label") &&
-            $('#column'+Number(j+2)+'_row'+x).hasClass("ui black circular label") &&
-            $('#column'+Number(j+3)+'_row'+x).hasClass("ui black circular label") &&
+        if ($('#column'+j+'_row'+i).hasClass("ui black circular label") &&
+            $('#column'+Number(j+1)+'_row'+i).hasClass("ui black circular label") &&
+            $('#column'+Number(j+2)+'_row'+i).hasClass("ui black circular label") &&
+            $('#column'+Number(j+3)+'_row'+i).hasClass("ui black circular label")
           ) {
               $('.ui.teal.compact.message').hide();
               $('.ui.center.aligned.icon.header').hide();
               disableButton();
               $('#winner').replaceWith("<h1><strong>Black Player Wins!</strong></h1>");
+              $('#status').replaceWith('Black Wins!');
           }
       }
     }
@@ -46,51 +48,98 @@ $(document).ready(function(){
 
   function checkVerticalConnect() {
     for(var j=1;j<8;j++) {
-      for(var i=6; i>3;x--) {
+      for(var i=6; i>3;i--) {
         if($('#column'+j+'_row'+i).hasClass("ui white circular label") &&
             $('#column'+j+'_row'+Number(i-1)).hasClass("ui white circular label") &&
             $('#column'+j+'_row'+Number(i-2)).hasClass("ui white circular label") &&
-            $('#column'+j+'_row'+Number(i-3)).hasClass("ui white circular label") &&
+            $('#column'+j+'_row'+Number(i-3)).hasClass("ui white circular label")
           ) {
               $('.ui.teal.compact.message').hide();
      	 				$('.ui.center.aligned.icon.header').hide();
      	 				disableButton();
      	 				$('#winner').replaceWith("<h1><strong>White Player Wins!</strong></h1>");
+              $('#status').replaceWith('White Wins!');
           }
 
         if($('#column'+j+'_row'+i).hasClass("ui black circular label") &&
             $('#column'+j+'_row'+Number(i-1)).hasClass("ui black circular label") &&
             $('#column'+j+'_row'+Number(i-2)).hasClass("ui black circular label") &&
-            $('#column'+j+'_row'+Number(i-3)).hasClass("ui black circular label") &&
+            $('#column'+j+'_row'+Number(i-3)).hasClass("ui black circular label")
           ) {
               $('.ui.teal.compact.message').hide();
               $('.ui.center.aligned.icon.header').hide();
               disableButton();
               $('#winner').replaceWith("<h1><strong>Black Player Wins!</strong></h1>");
+              $('#status').replaceWith('Black Wins!');
           }
       }
     }
   }
 
   function checkDiagonalLeftConnect() {
-    for(var j=7;j>3;y--) {
-      for(var i=6;i>3;x--) {
+    for(var j=7;j>3;j--) {
+      for(var i=6;i>3;i--) {
+        if($('#column'+j+'_row'+i).hasClass("ui white circular label") &&
+            $('#column'+Number(j-1)+'_row'+Number(i-1)).hasClass("ui white circular label") &&
+            $('#column'+Number(j-2)+'_row'+Number(i-2)).hasClass("ui white circular label") &&
+            $('#column'+Number(j-3)+'_row'+Number(i-3)).hasClass("ui white circular label")
+          ) {
+              $('.ui.teal.compact.message').hide();
+              $('.ui.center.aligned.icon.header').hide();
+              disableButton();
+              $('#winner').replaceWith("<h1><strong>White Player Wins!</strong></h1>");
+              $('#status').replaceWith('White Wins!');
+          }
 
+        if($('#column'+j+'_row'+i).hasClass("ui black circular label") &&
+            $('#column'+Number(j-1)+'_row'+Number(i-1)).hasClass("ui black circular label") &&
+            $('#column'+Number(j-2)+'_row'+Number(i-2)).hasClass("ui black circular label") &&
+            $('#column'+Number(j-3)+'_row'+Number(i-3)).hasClass("ui black circular label")
+          ) {
+              $('.ui.teal.compact.message').hide();
+              $('.ui.center.aligned.icon.header').hide();
+              disableButton();
+              $('#winner').replaceWith("<h1><strong>Black Player Wins!</strong></h1>");
+              $('#status').replaceWith('Black Wins!');
+          }
       }
     }
   }
 
   function checkDiagonalRightConnect() {
-    for(var j=1;j<3;y++) {
-      for(var i=6;x>3;x--) {
+    for(var j=1;j<3;j++) {
+      for(var i=6;i>3;i--) {
+        if($('#column'+j+'_row'+i).hasClass("ui white circular label") &&
+            $('#column'+Number(j+1)+'_row'+Number(i-1)).hasClass("ui white circular label") &&
+            $('#column'+Number(j+2)+'_row'+Number(i-2)).hasClass("ui white circular label") &&
+            $('#column'+Number(j+3)+'_row'+Number(i-3)).hasClass("ui white circular label")
+          ) {
+              $('.ui.teal.compact.message').hide();
+              $('.ui.center.aligned.icon.header').hide();
+              disableButton();
+              $('#winner').replaceWith("<h1><strong>White Player Wins!</strong></h1>");
+              $('#status').replaceWith('White Wins!');
+          }
 
+        if($('#column'+j+'_row'+i).hasClass("ui black circular label") &&
+            $('#column'+Number(j+1)+'_row'+Number(i-1)).hasClass("ui black circular label") &&
+            $('#column'+Number(j+2)+'_row'+Number(i-2)).hasClass("ui black circular label") &&
+            $('#column'+Number(j+3)+'_row'+Number(i-3)).hasClass("ui black circular label")
+          ) {
+              $('.ui.teal.compact.message').hide();
+              $('.ui.center.aligned.icon.header').hide();
+              disableButton();
+              $('#winner').replaceWith("<h1><strong>Black Player Wins!</strong></h1>");
+              $('#status').replaceWith('Black Wins!');
+          }
       }
     }
   }
 
   function disableButton() {
-    for (var i=1;i<8;i++) {
-      $('#column'+i+'_button').addClass('disabled');
+    var names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh'];
+    for (var i=0;i<names.length;i++) {
+      $('#'+names[i]+'_column_button').addClass('disabled');
     }
   }
 
--- app/controllers/arena_controller.rb
+++ app/controllers/arena_controller.rb
@@ -2,30 +2,30 @@ class ArenaController < ApplicationController
   before_action :find_white_player
 
   def reload
-    %w(First Second Third Fourth Fifth Sixth Seventh).map do |name|
-      "#{name}Column".constantize.where(take: true).update_all(take:false)
+    @white_player.update(active: true)
+    %w(first second third fourth fifth sixth seventh).map do |ordinal|
+      "#{ordinal.capitalize}Column".constantize.where(take: true).update_all(take: false)
+      "#{ordinal.capitalize}Column".constantize.where(white_player: true).update_all(white_player: false)
     end
 
-    @white_player.update(active: true)
     flash[:notice] = "Game Reloaded"
     redirect_to root_url
   end
 
-  %w(first second third fourth fifth sixth seventh).map do |name|
-    define_method "drop_cap_to_#{name}_column" do
-      @rows = "#{name.capitalize}Column".constantize.all.order("id DESC")
-    	@rows.each do |row|
-    	  if !row.take
-    	 	present_row = row
-    		present_row.update(take: true)
-    		  if @white_player.active?
-    		  	present_row.update(white_player: true)
-    		  	@white_player.update(active: false)
-    		  else
-    		  	present_row.update(white_player: false)
-    		  	@white_player.update(active: true)
-    		  end
-    		break
+  %w(first second third fourth fifth sixth seventh).map do |ordinal|
+    define_method "drop_cap_to_#{ordinal}_column" do
+      @rows = "#{ordinal.capitalize}Column".constantize.all.order("id DESC")
+    	@rows.each do |present_row|
+        unless present_row.take
+      		present_row.update(take: true)
+      		  if @white_player.active?
+      		  	present_row.update(white_player: true)
+      		  	@white_player.update(active: false)
+      		  else
+      		  	present_row.update(white_player: false)
+      		  	@white_player.update(active: true)
+      		  end
+      		break
     	  end
     	end
     	flash[:notice] = "Change Turn"
